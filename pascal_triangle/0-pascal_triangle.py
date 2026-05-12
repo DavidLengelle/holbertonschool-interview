8f070303507dd5d4c40de65aba87e81ec1da5b51
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-12 15:45:23 +0200

    add function to generate Pascal's triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Pascal Triangle module"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of n"""
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(n - 1):
+        new_row = [1]
+        prev_row = triangle[-1]
+
+        for j in range(len(prev_row) - 1):
+            new_row.append(prev_row[j] + prev_row[j + 1])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
