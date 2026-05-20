49c363b4479014c8eafc6317045ae50735feb216
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-20 14:33:51 +0200

    add function to determine if all lockboxes can be opened

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lockboxes module"""
+
+
+def canUnlockAll(boxes):
+    """Return True if all the boxes can be opened, False otherwise"""
+    
+    opened = {0}
+    to_check = [0]
+
+    while len(to_check) > 0:
+        current = to_check.pop()
+        for key in boxes[current]:
+            if key < len(boxes) and key not in opened:
+                opened.add(key)
+                to_check.append(key)
+
+    if len(opened) == len(boxes):
+        return True
+    else:
+        return False
