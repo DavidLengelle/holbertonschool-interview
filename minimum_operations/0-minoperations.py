9ebddc44b8db35642fb3120eb26cd4e0e22dbd4f
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-27 10:34:21 +0200

    add minOperations function to calculate minimum operations for a given number

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+def minOperations(n):
+
+    operations = 0
+    d = 2
+
+    if n <= 1:
+        return 0
+
+    while n > 1:
+        while n % d == 0:
+            operations += d
+            n = n // d
+        d += 1
+    return operations
