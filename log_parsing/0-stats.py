34c2b588e7087dbbd38a1a0539b02d45ed9a0f80
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-05 11:26:30 +0200

    fix: ensure final stats are printed after processing all input lines

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -39,6 +39,8 @@ if __name__ == "__main__":
             if count % 10 == 0:
                 print_stats(total_size, status_counts)
 
+        print_stats(total_size, status_counts)
+
     except KeyboardInterrupt:
         print_stats(total_size, status_counts)
         raise
